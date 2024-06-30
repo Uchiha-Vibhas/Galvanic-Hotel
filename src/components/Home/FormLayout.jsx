6e1eb5bbb92fab4841bb257ff2675423e362f078
Spidey6162099 <141052710+Spidey6162099@users.noreply.github.com>
2024-06-30 15:50:41 +0530

    todo:create a add Customers Page

--- src/components/Home/FormLayout.jsx
+++ src/components/Home/FormLayout.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Form = () => {
+  const [User, setUser] = useState({ username: '', email: '', password: '' });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Make a POST request to your Spring Boot server
+
+      await axios.post('http://localhost:8080/Galvanic/login',User);
+
+    } catch (error) {
+
+    }
+  };
   return (
     <section className="bg-gray-900 w-full">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -18,23 +38,39 @@ const Form = () => {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create an account
+              Welcome
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              {/*<div>*/}
+              {/*  <label*/}
+              {/*    htmlFor="email"*/}
+              {/*    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"*/}
+              {/*  >*/}
+              {/*    Your email*/}
+              {/*  </label>*/}
+              {/*  <input*/}
+              {/*    type="email"*/}
+              {/*    name="email"*/}
+              {/*    id="email"*/}
+              {/*    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"*/}
+              {/*    placeholder="name@company.com"*/}
+              {/*    required=""*/}
+              {/*  />*/}
+              {/*</div>*/}
               <div>
                 <label
-                  htmlFor="email"
+                  htmlFor="username"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your email
+                  username
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Uchiha Vibhas"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
+                  required
                 />
               </div>
               <div>
@@ -42,7 +78,7 @@ const Form = () => {
                   htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Password
+                  password
                 </label>
                 <input
                   type="password"
@@ -50,23 +86,7 @@ const Form = () => {
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Confirm password
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required
                 />
               </div>
               <div className="flex items-start">
@@ -76,7 +96,7 @@ const Form = () => {
                     aria-describedby="terms"
                     type="checkbox"
                     className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
+                    required
                   />
                 </div>
                 <div className="ml-3 text-sm">
@@ -98,17 +118,17 @@ const Form = () => {
                 type="submit"
                 className="w-full  text-white bg-gray-800 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 shadow-md"
               >
-                Create an account
+                Login
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
-                <a
-                  href="#"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Login here
-                </a>
-              </p>
+              {/*<p className="text-sm font-light text-gray-500 dark:text-gray-400">*/}
+              {/*  Already have an account?{" "}*/}
+              {/*  <a*/}
+              {/*    href="#"*/}
+              {/*    className="font-medium text-primary-600 hover:underline dark:text-primary-500"*/}
+              {/*  >*/}
+              {/*    Login here*/}
+              {/*  </a>*/}
+              {/*</p>*/}
             </form>
           </div>
         </div>
