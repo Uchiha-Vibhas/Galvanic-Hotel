d07cf2c0042f285d5e72c8194e9d2b40eb479edb
Spidey6162099 <141052710+Spidey6162099@users.noreply.github.com>
2024-07-03 11:43:25 +0530

    Merge branch 'main' of https://github.com/Uchiha-Vibhas/Galvanic-Hotel

--- src/components/Home/FormLayout.jsx
+++ src/components/Home/FormLayout.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 
 const Form = () => {
+  const [User, setUser] = useState({ username: "", password: "" });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Make a POST request to your Spring Boot server
+
+    const response = await axios.post(
+      "http://localhost:8080/Galvanic/login",
+      User
+    );
+    if (response.status == 200) {
+      console.log("logged in succesfully");
+      const content = await axios.get("http://localhost:8080/Galvanic/all");
+      console.log(content.data);
+    } else {
+      console.log("logged in failed");
+    }
+  };
   return (
     <section className="bg-gray-900 w-full">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -33,8 +57,9 @@ const Form = () => {
                   name="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
+                  required
+                  value={User.username}
+                  onChange={handleInputChange}
                 />
               </div>
               <div>
@@ -50,23 +75,9 @@ const Form = () => {
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Confirm password
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required
+                  value={User.password}
+                  onChange={handleInputChange}
                 />
               </div>
               <div className="flex items-start">
@@ -76,7 +87,7 @@ const Form = () => {
                     aria-describedby="terms"
                     type="checkbox"
                     className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
+                    required
                   />
                 </div>
                 <div className="ml-3 text-sm">
